3732aff400a4932c245eb3cf183188a0786254f1
Alex <suarez9093@hotmail.com>
2019-07-24 20:10:41 -0600

    created all components of game. All functioning and loading from App.js

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import ToyCards from "./components/ToyCards";
+import Footer from "./components/Footer";
+
 function App(){
-    return (<Header />)
+    return (
+    <div>
+    <Navbar />
+    <Header />
+    <ToyCards />
+    <Footer />
+    </div>)
+
 }
 
 export default App;
