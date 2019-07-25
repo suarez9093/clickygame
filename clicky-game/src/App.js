ff2f274656cce92f92de1fc5e4b0fbf207586456
Alex <suarez9093@hotmail.com>
2019-07-24 21:10:29 -0600

    functioning components that are generating toys from the toys.json. Image is not displaying

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import ToyCards from "./components/ToyCards";
+import ToyCard from "./components/ToyCard";
 import Footer from "./components/Footer";
+import toys from "./toys.json";
 
-function App(){
-    return (
-    <div>
-    <Navbar />
-    <Header />
-    <ToyCards />
-    <Footer />
-    </div>)
+class App extends Component {
+
+    state = {
+        toys
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Header />
+                {this.state.toys.map(toy => (
+                    <ToyCard
+                        id={toy.id}
+                        name={toy.name}
+                        image={toy.image}
+                        role={toy.role}
+                    />
+                ))}
+                <Footer />
+            </div>
+        );
+    }
 
 }
 
