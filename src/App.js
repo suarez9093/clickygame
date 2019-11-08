e5ff95a4a7e2fd15504184f10596b356bda6a095
Alex <suarez9093@hotmail.com>
2019-11-07 23:30:53 -0700

    added music

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
                         />
                     ))}
                 </Wrapper>
-                {/* <Music /> */}
+                <Music />
             </div>
         );
     }
